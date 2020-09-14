413972bfb7d78a90df2e044571039788ed2a1ecb
Jordan Cheng <51500782+jordancheng604@users.noreply.github.com>
2020-09-14 16:51:05 -0700

    Client front-end -- build prod folder

---
+++ client/build/precache-manifest.74b2247eec9dfde7d30cc2784a97b5e0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676966ff131a01e7f6d88677e6034ae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed4c4a662107753250f",
+    "url": "/static/css/main.1f18ad71.chunk.css"
+  },
+  {
+    "revision": "0f2f49fdd9b9aed54dbb",
+    "url": "/static/js/2.465277bd.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.465277bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed4c4a662107753250f",
+    "url": "/static/js/main.fbe06b3d.chunk.js"
+  },
+  {
+    "revision": "3b15450bc80be2bd143a",
+    "url": "/static/js/runtime-main.146d68da.js"
+  },
+  {
+    "revision": "ed29f7145ef6c8b9aba31c4c307291c4",
+    "url": "/static/media/Icon-add.ed29f714.svg"
+  },
+  {
+    "revision": "5367890b6127ac9194da8b456503068b",
+    "url": "/static/media/Icon-back-arrow.5367890b.svg"
+  },
+  {
+    "revision": "8abaef0c13a9cd0874034de4a400763f",
+    "url": "/static/media/Icon-kebab-default.8abaef0c.svg"
+  },
+  {
+    "revision": "286069001111ea3ed233483011ce0c09",
+    "url": "/static/media/Icon-search.28606900.svg"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "d69cdef70f4bb054ccc674cd9d995ddd",
+    "url": "/static/media/Logo-instock.d69cdef7.svg"
+  }
+]);
