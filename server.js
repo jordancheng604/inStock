194f82ed3dbd83a48e57cef4058cd9dc14785027
jordancheng604 <jordancheng604@gmail.com>
2020-09-15 12:02:21 -0700

    Added an app.get with a welcome message.

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(cors());
 
 app.use(express.urlencoded({extended: false}));
 
+app.get('/', (_req,res,next)=>{
+    res.send("Hi there and Welcome to inStock, a project that involved a team of four people. You are viewing this from Jordan's version (www.jordancheng.com)")
+    next()
+})
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
