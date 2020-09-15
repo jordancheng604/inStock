742682b78a39f627505971fbb19209e27303f85a
jordancheng604 <jordancheng604@gmail.com>
2020-09-15 11:56:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
 const cors = require("cors");
+const inventory_routes = require("./routes/inventory.js");
+const warehouses_routes = require("./routes/warehouse.js");
 
 // bodyparser
 app.use(express.json());
 // CORS
 app.use(cors());
 
+app.use(express.urlencoded({extended: false}));
+
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-const inventory_routes = require("./routes/inventory.js");
-
-const warehouses_routes = require("./routes/warehouse.js");
 // inventory route
 app.use("/inventory", inventory_routes);
 // warehouse route
