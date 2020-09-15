a3ca2f02cf4a904a4497d37f14b0441786254e3f
jordancheng604 <jordancheng604@gmail.com>
2020-09-15 11:53:03 -0700

    server.js editing rearranging code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const PORT = process.env.PORT || 5000;
 const app = express();
 const cors = require("cors");
 
@@ -7,12 +8,12 @@ app.use(express.json());
 // CORS
 app.use(cors());
 
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
 const inventory_routes = require("./routes/inventory.js");
+
 const warehouses_routes = require("./routes/warehouse.js");
 // inventory route
 app.use("/inventory", inventory_routes);
 // warehouse route
 app.use("/warehouses", warehouses_routes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
